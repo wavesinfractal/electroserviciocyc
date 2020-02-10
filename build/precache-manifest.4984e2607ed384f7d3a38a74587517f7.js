c6e4ae694063b8466089e6de323edbf450503953
Frank <wavesinfractal@gmail.com>
2020-02-10 14:24:53 -0600

    se aplica el build

---
+++ build/precache-manifest.4984e2607ed384f7d3a38a74587517f7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d15684354b27941240dc349e507f0554",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4f68b3a95bd4e57e9b4f",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "e4d439a4c51fab5ecdf1",
+    "url": "/static/js/2.e129b4e9.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.e129b4e9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4f68b3a95bd4e57e9b4f",
+    "url": "/static/js/main.8c8829a8.chunk.js"
+  },
+  {
+    "revision": "d507ed2dd482491dec6f",
+    "url": "/static/js/runtime-main.e1ff0ad5.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
